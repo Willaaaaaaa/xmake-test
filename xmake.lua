3e66bfb764056e18cd7d08090f811c84ef3a3b49
Willaaaaaaa <willaaaaaaa936@gmail.com>
2026-01-17 11:54:37 +0800

    test xmake ncnn with vk

---
+++ xmake.lua
@@ -0,0 +1,19 @@
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
+
+includes("packages/*.lua")
+
+option("ncnn_ver", {default = "undefined", showmenu = true, description = "ncnn version"})
+add_options("ncnn_ver")
+
+if is_config("ncnn_ver", "20260113") then
+    add_requires("my-ncnn[vulkan,simpleocv]" .. " 20260113")
+elseif is_config("ncnn_ver", "20250916") then
+    add_requires("my-ncnn[vulkan,simpleocv]" .. " 20250916")
+else
+    -- "undefined"
+end
+
+target("xmake-test")
+    add_files("ncnn.cc")
+    add_packages("my-ncnn")
