b347e9cf1766897117afb5c960febecf995f3926
Willaaaaaaa <willaaaaaaa936@gmail.com>
2026-01-24 15:22:20 +0800

    t

--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,15 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
 
-includes("packages/*.lua")
+add_repositories("ncnn-test git@github.com:Willaaaaaaa/xmake-repo.git patch-260116")
+
+-- includes("packages/*.lua")
 
 option("ncnn_ver", {default = "undefined", showmenu = true, description = "ncnn version"})
 add_options("ncnn_ver")
 
 if has_config("ncnn_ver") then
-    add_requires("my-ncnn[vulkan,simpleocv,shared=" .. (is_kind("shared") and "true" or "false") .. "] " .. get_config("ncnn_ver"))
+    add_requires("ncnn[vulkan,simpleocv,shared=" .. (is_kind("shared") and "true" or "false") .. "] " .. get_config("ncnn_ver"))
 else
     -- "undefined"
 end
@@ -22,4 +24,4 @@ add_requireconfs("**.python 3.x", {
 
 target("xmake-test")
     add_files("ncnn.cc")
-    add_packages("my-ncnn")
+    add_packages("ncnn")
